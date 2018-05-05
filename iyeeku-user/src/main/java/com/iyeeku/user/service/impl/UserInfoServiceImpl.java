e917cc85c1a34bf73fd1a4801f99d82f2b5019b2
Stephen-Y <iyeeku@qq.com>
2018-05-05 11:44:10 +0800

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: Stephen-Y <iyeeku@qq.com>
    
    # Conflicts:
    #	iyeeku-user/src/main/java/com/iyeeku/user/dao/impl/UserInfoDaoImpl.java
    #	iyeeku-user/src/main/java/com/iyeeku/user/service/impl/UserInfoServiceImpl.java
    #	iyeeku-webapp/src/main/resources/config/env/log4j_iyeeku.xml
    #	iyeeku-webapp/src/main/resources/config/mybatis/frame/base-config.xml

--- iyeeku-user/src/main/java/com/iyeeku/user/service/impl/UserInfoServiceImpl.java
+++ iyeeku-user/src/main/java/com/iyeeku/user/service/impl/UserInfoServiceImpl.java
@@ -3,6 +3,8 @@ package com.iyeeku.user.service.impl;
 import com.iyeeku.user.dao.IUserInfoDao;
 import com.iyeeku.user.service.IUserInfoService;
 import com.iyeeku.user.vo.UserInfoVO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,21 +13,20 @@ import java.util.List;
 @Service
 public class UserInfoServiceImpl implements IUserInfoService {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserInfoServiceImpl.class);
+
     @Autowired
     private IUserInfoDao iUserInfoDao;
 
-    public void setiUserInfoDao(IUserInfoDao iUserInfoDao) {
-        this.iUserInfoDao = iUserInfoDao;
-    }
-
     @Override
     public List<UserInfoVO> getAllUserInfos(UserInfoVO userInfoVO) {
+        logger.info("IUserInfoService getAllUserInfos");
         return this.iUserInfoDao.getAllUserInfos(userInfoVO);
     }
 
     @Override
     public void register(UserInfoVO userInfoVO) {
-
+        logger.info("IUserInfoService register");
         this.iUserInfoDao.save(userInfoVO);
 
     }
